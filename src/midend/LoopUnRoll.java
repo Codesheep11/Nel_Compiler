4896c4474c0d4731c69cfa59e65a71c95b6b31c2
CodeSheep11 <3139784737@qq.com>
2024-06-17 20:40:21 +0800

    fix:LoopUnRoll

--- src/midend/LoopUnRoll.java
+++ src/midend/LoopUnRoll.java
@@ -1,2 +1,29 @@
-package midend;public class LoopUnroll {
-}
+//package midend;
+//
+//import mir.*;
+//import mir.Module;
+//
+///**
+// * based on LCSSA /<br>
+// */
+//public class LoopUnRoll {
+//    private static int UnRollLine = 3000;
+//
+//    public static void run(Module module) {
+//        for (Function function : module.getFuncSet()) {
+//            loopUnRollForFunc(function);
+//        }
+//    }
+//
+//    public static void loopUnRollForFunc(Function function) {
+//        for (Loop loop : function.loops) {
+//            loopUnRollForLoop(loop);
+//        }
+//    }
+//
+//    public static void loopUnRollForLoop(Loop loop) {
+//        //先对子循环进行处理
+//
+//    }
+//
+//}
