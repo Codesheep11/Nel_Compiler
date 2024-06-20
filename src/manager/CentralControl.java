05a5161f4cf056bf19cac3291b3175b740a5d8e7
Srchycz <21373472@buaa.edu.cn>
2024-06-20 19:56:22 +0800

    perf: 优化GVN结构, 支持替换CALL

--- src/manager/CentralControl.java
+++ src/manager/CentralControl.java
@@ -1,7 +1,9 @@
 package manager;
 
 public class CentralControl {
-
+    // 用于控制浮点数精度
+    public static double epsilon = 1e-6;
+    // 用于控制是否开启各种优化
     public static boolean _GVN_OPEN = true;
 
     public static boolean _GCM_OPEN = true;
--- src/midend/GlobalValueNumbering.java
+++ src/midend/GlobalValueNumbering.java
@@ -68,12 +68,9 @@ public class GlobalValueNumbering {
     }
 
     private static String generateExpressionKey(Instruction inst) {
-        switch (inst.getInstType()) {
+        return switch (inst.getInstType()) {
             // 满足交换律的操作符
-            case FAdd:
-            case FMUL:
-            case ADD:
-            case MUL: {
+            case FAdd, FMUL, ADD, MUL -> {
                 Instruction.BinaryOperation binaryOperation = (Instruction.BinaryOperation) inst;
                 String operand1 = binaryOperation.getOperand_1().getDescriptor();
                 String operand2 = binaryOperation.getOperand_2().getDescriptor();
@@ -83,28 +80,22 @@ public class GlobalValueNumbering {
                     operand1 = operand2;
                     operand2 = temp;
                 }
-                return inst.getInstType().name() + "," + operand1 + "," + operand2;
+                yield inst.getInstType().name() + "," + operand1 + "," + operand2;
             }
             // 不满足交换律的操作符
-            case FREM:
-            case FSUB:
-            case FDIV:
-            case REM:
-            case SUB:
-            case DIV: {
+            case FREM, FSUB, FDIV, REM, SUB, DIV -> {
                 Instruction.BinaryOperation binaryOperation = (Instruction.BinaryOperation) inst;
                 String operand1 = binaryOperation.getOperand_1().getDescriptor();
                 String operand2 = binaryOperation.getOperand_2().getDescriptor();
-                return inst.getInstType().name() + "," + operand1 + "," + operand2;
+                yield inst.getInstType().name() + "," + operand1 + "," + operand2;
             }
-            case Fcmp:
-            case Icmp: {
+            case Fcmp, Icmp -> {
                 Instruction.Condition compare = (Instruction.Condition) inst;
                 String operand1 = compare.getSrc1().getDescriptor();
                 String operand2 = compare.getSrc2().getDescriptor();
-                return compare.getCmpOp() + "," + operand1 + "," + operand2;
+                yield compare.getCmpOp() + "," + operand1 + "," + operand2;
             }
-            case GEP: {
+            case GEP -> {
                 Instruction.GetElementPtr gep = (Instruction.GetElementPtr) inst;
                 String base = gep.getBase().getDescriptor();
                 StringBuilder indices = new StringBuilder();
@@ -112,21 +103,27 @@ public class GlobalValueNumbering {
                     indices.append(val.getDescriptor());
                     indices.append(",");
                 }
-                return inst.getInstType() + "," + base + "," + indices;
+                yield inst.getInstType() + "," + base + "," + indices;
             }
-            case Zext: {
-                return inst.getInstType().name() + "," + inst.getOperands().get(0).toString() + "," + inst.getType();
+            case Zext -> inst.getInstType().name() + "," + inst.getOperands().get(0).toString() + "," + inst.getType();
+            case CALL -> {
+                Instruction.Call call = (Instruction.Call) inst;
+                StringBuilder args = new StringBuilder();
+                for (Value val : call.getOperands()) {
+                    args.append(val.getDescriptor());
+                    args.append(",");
+                }
+                yield inst.getInstType() + "," + call.getDestFunction().getDescriptor() + "," + args;
             }
-            default: {
+            default -> {
                 System.out.println("Warning: GVN 未处理类型: " + inst.getInstType() + "!");
-                return inst.toString();
+                yield inst.toString();
             }
-        }
+        };
     }
 
     /**
-     * 常量折叠 <br>
-     * NOTE: 现在只折叠了运算结果为 int32 和 float32的指令，之后可以考虑 int1 是否可以折叠
+     * 常量折叠
      */
     private static boolean tryConstantFolding(Instruction instruction) {
         if (instruction instanceof Instruction.BinaryOperation binaryOperation) {
@@ -162,6 +159,44 @@ public class GlobalValueNumbering {
                         }
                     }
                     instruction.replaceAllUsesWith(new Constant.ConstantFloat(result));
+                    return true;
+                }
+            }
+        } else if (instruction instanceof Instruction.Condition condition) {
+            Value operand1 = condition.getSrc1();
+            Value operand2 = condition.getSrc2();
+            if (operand1 instanceof Constant op1 && operand2 instanceof Constant op2) {
+                if (condition instanceof Instruction.Icmp) {
+                    int val1 = (int) op1.getConstValue();
+                    int val2 = (int) op2.getConstValue();
+                    boolean result = false;
+                    switch (condition.getCmpOp()) {
+                        case "eq" -> result = (val1 == val2);
+                        case "ne" -> result = (val1 != val2);
+                        case "slt" -> result = (val1 < val2);
+                        case "sle" -> result = (val1 <= val2);
+                        case "sgt" -> result = (val1 > val2);
+                        case "sge" -> result = (val1 >= val2);
+                        default -> {
+                        }
+                    }
+                    instruction.replaceAllUsesWith(new Constant.ConstantBool(result ? 1 : 0));
+                    return true;
+                } else if (condition instanceof Instruction.Fcmp) {
+                    float val1 = (float) op1.getConstValue();
+                    float val2 = (float) op2.getConstValue();
+                    boolean result = false;
+                    switch (condition.getCmpOp()) {
+                        case "oeq" -> result = (Math.abs(val1 - val2) < CentralControl.epsilon);
+                        case "one" -> result = (Math.abs(val1 - val2) >= CentralControl.epsilon);
+                        case "olt" -> result = (val1 < val2);
+                        case "ole" -> result = (val1 < val2 || Math.abs(val1 - val2) < CentralControl.epsilon);
+                        case "ogt" -> result = (val1 > val2);
+                        case "oge" -> result = (val1 > val2 || Math.abs(val1 - val2) < CentralControl.epsilon);
+                        default -> {
+                        }
+                    }
+                    instruction.replaceAllUsesWith(new Constant.ConstantBool(result ? 1 : 0));
                     return true;
                 }
             }
