3691e763b1304fb8f7e624d522da56d8ba282e7f
何立群 <21373471@buaa.edu.com>
2024-07-12 21:07:34 +0800

    addcal

---
+++ Scheduler.java
@@ -0,0 +1,124 @@
+package mir.Ir2RiscV;
+
+import backend.operand.Address;
+import backend.operand.Reg;
+import backend.riscv.RiscvFloat;
+import backend.riscv.RiscvInstruction.LS;
+import backend.riscv.RiscvInstruction.La;
+import backend.riscv.RiscvInstruction.Li;
+import backend.riscv.RiscvInstruction.R2;
+import mir.Constant;
+import mir.Function;
+import mir.Type;
+import mir.Value;
+
+import java.util.HashMap;
+
+public class VirRegMap {
+    public static final VirRegMap VRM = new VirRegMap();
+
+    public final HashMap<Value, Reg> map = new HashMap<>();
+
+    private Function nowFunction;
+
+    public VirRegMap() {
+
+    }
+
+    public void clean(Function function) {
+        this.nowFunction = function;
+        map.clear();
+    }
+
+    public Reg genReg(Value value) {
+        Type type = value.getType();
+        Reg reg;
+        if (type.isFloatTy()) {
+            reg = Reg.getVirtualReg(Reg.RegType.FPR, 32);
+        } else if (type.isInt64Ty() || type.isPointerTy()) {
+            reg = Reg.getVirtualReg(Reg.RegType.GPR, 64);
+        } else if (type.isInt1Ty() || type.isInt32Ty()) {
+            reg = Reg.getVirtualReg(Reg.RegType.GPR, 32);
+        } else {
+            throw new RuntimeException("alloc array to a reg");
+        }
+        return reg;
+    }
+
+    public void addValue(Value value) {
+        Type type = value.getType();
+        Reg reg;
+        if (type.isFloatTy()) {
+            reg = Reg.getVirtualReg(Reg.RegType.FPR, 32);
+        } else if (type.isInt64Ty() || type.isPointerTy()) {
+            reg = Reg.getVirtualReg(Reg.RegType.GPR, 64);
+        } else if (type.isInt1Ty() || type.isInt32Ty()) {
+            reg = Reg.getVirtualReg(Reg.RegType.GPR, 32);
+        } else {
+            throw new RuntimeException("alloc array to a reg");
+        }
+        // bits:位数，64，32?
+        map.put(value, reg);
+    }
+
+    // 不知道是否在前面的指令中被建立了,该方法是传入一个value，如果已经有了虚拟寄存器则返回，否则现场分配一个然后返回
+    // 当然,需要保证如果是float或者int常量的话需要提前把其中数值加到寄存器中
+    // 需要额外保证所有的存到栈中的寄存器arg均要在这个阶段被从地址中拿到值
+    public Reg ensureRegForValue(Value value) {
+        if (nowFunction.isAddressArg(value)) {
+            addValue(value);
+            Reg reg = map.get(value);
+            Reg sp = Reg.getPreColoredReg(Reg.PhyReg.sp, 64);
+            Address address = nowFunction.getArgAddress((Function.Argument) value);
+            if (value.getType().isFloatTy()) {
+                CodeGen.nowBlock.riscvInstructions.addLast(new LS(CodeGen.nowBlock, reg, sp, address, LS.LSType.flw));
+            } else if (value.getType().isInt64Ty() || value.getType().isPointerTy()) {
+                CodeGen.nowBlock.riscvInstructions.addLast(new LS(CodeGen.nowBlock, reg, sp, address, LS.LSType.ld));
+            } else if (value.getType().isInt32Ty()) {
+                CodeGen.nowBlock.riscvInstructions.addLast(new LS(CodeGen.nowBlock, reg, sp, address, LS.LSType.lw));
+            } else {
+                throw new RuntimeException("wrong type");
+            }
+            return reg;
+        } else if (value instanceof Constant) {
+            if ((value instanceof Constant.ConstantInt && (Integer) ((Constant.ConstantInt) value).getConstValue() == 0)
+                    || (value instanceof Constant.ConstantBool) && (Integer) ((Constant.ConstantBool) value).getConstValue() == 0) {
+                return Reg.getPreColoredReg(Reg.PhyReg.zero, 32);
+            }
+            Reg reg = genReg(value);
+            if (value instanceof Constant.ConstantFloat) {
+                Float init = ((Float) ((Constant.ConstantFloat) value).getConstValue());
+                RiscvFloat rf = CodeGen.ansRis.getSameFloat(init);
+                Reg tmp = Reg.getPreColoredReg(Reg.PhyReg.t0, 64);
+                CodeGen.nowBlock.riscvInstructions.addLast(new La(CodeGen.nowBlock, tmp, rf));
+                CodeGen.nowBlock.riscvInstructions.addLast(new LS(CodeGen.nowBlock, reg, tmp, new Address(0), LS.LSType.flw));
+            } else if (value instanceof Constant.ConstantInt) {
+                int init = ((Integer) ((Constant.ConstantInt) value).getConstValue());
+                CodeGen.nowBlock.riscvInstructions.addLast(new Li(CodeGen.nowBlock, reg, init));
+            } else if (value instanceof Constant.ConstantBool) {
+                int init = ((Integer) ((Constant.ConstantBool) value).getConstValue());
+                CodeGen.nowBlock.riscvInstructions.addLast(new Li(CodeGen.nowBlock, reg, init));
+            } else {
+                throw new RuntimeException("wrong const Type");
+            }
+            return reg;
+        } else {
+            if (!map.containsKey(value)) {
+                addValue(value);
+            }
+            return map.get(value);
+        }
+    }
+
+    // b的绑定为a的寄存器
+    public void binding(Value a, Value b) {
+        Reg reg = ensureRegForValue(a);
+        //绑定在控制流变化后会有bug，如果这个本身的寄存器提前出现呢?
+        //所以答案是，提前先看看原本有没有这个寄存器
+        if (map.containsKey(b)) {
+            CodeGen.nowBlock.riscvInstructions.addLast(new R2(CodeGen.nowBlock, map.get(b), reg, R2.R2Type.mv));
+        } else {
+            map.put(b, reg);
+        }
+    }
+}
---
+++ src/backend/Opt/CalculateOpt.java
@@ -0,0 +1,179 @@
+package backend.Opt;
+
+import backend.operand.Imm;
+import backend.operand.Reg;
+import backend.riscv.RiscvBlock;
+import backend.riscv.RiscvFunction;
+import backend.riscv.RiscvGlobalVar;
+import backend.riscv.RiscvInstruction.*;
+import backend.riscv.RiscvModule;
+import utils.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CalculateOpt {
+    public static void run(RiscvModule riscvModule) {
+        for (RiscvFunction function : riscvModule.funcList) {
+            liAdd2Addi(function);
+            for (RiscvBlock block : function.blocks) {
+                ConstValueReUse(block);
+                ConstPointerReUse(block);
+            }
+        }
+    }
+
+    //
+    public static void liAdd2Addi(RiscvFunction function) {
+        for (RiscvBlock block : function.blocks) {
+            ArrayList<RiscvInstruction> newList = new ArrayList<>();
+            for (int i = 0; i < block.riscvInstructions.getSize(); i++) {
+                RiscvInstruction now = block.riscvInstructions.get(i);
+                if (i != block.riscvInstructions.getSize() - 1) {
+                    RiscvInstruction next = block.riscvInstructions.get(i + 1);
+                    if (now instanceof Li && next instanceof R3) {
+                        Reg liReg = ((Li) now).reg;
+                        Reg opRs1 = (Reg) ((R3) next).rs1;
+                        Reg opRs2 = (Reg) ((R3) next).rs2;
+                        R3 opi = null;
+                        if (((R3) next).type == R3.R3Type.addw) {
+                            if (((Li) now).imm >= -2047 && ((Li) now).imm <= 2047) {
+                                // 如果li的寄存器和addw用的寄存器之一重合
+                                if (liReg.equals(opRs1)) {
+                                    opi = new R3(block, ((R3) next).rd, opRs2, new Imm(((Li) now).imm), R3.R3Type.addi);
+                                } else if (liReg.equals(opRs2)) {
+                                    opi = new R3(block, ((R3) next).rd, opRs1, new Imm(((Li) now).imm), R3.R3Type.addi);
+                                }
+                                if (opi != null) {
+                                    newList.add(opi);
+                                    i++;// 跳过next
+                                    continue;
+                                }
+                            }
+                        } else if (((R3) next).type == R3.R3Type.subw) {
+                            if (((Li) now).imm >= -2047 && ((Li) now).imm <= 2047) {
+                                // 如果li的寄存器和sub的减的寄存器重合，因为如果被减的话需要反转另一个参数
+                                if (liReg.equals(opRs2)) {
+                                    opi = new R3(block, ((R3) next).rd, opRs1, new Imm(((Li) now).imm * -1), R3.R3Type.addi);
+                                    newList.add(opi);
+                                    i++;// 跳过next
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+                newList.add(now);
+            }
+            block.riscvInstructions.setEmpty();
+            for (RiscvInstruction instr : newList) {
+                block.riscvInstructions.addLast(instr);
+            }
+        }
+    }
+
+
+    // 重要依据，这些拉取的常数和全局的不会跨过块
+    public static void ConstValueReUse(RiscvBlock riscvBlock) {
+        final int range = 10;
+        HashMap<Integer, Pair<Reg, Integer>> map = new HashMap<>();
+        ArrayList<RiscvInstruction> newList = new ArrayList<>();
+        for (int i = 0; i < riscvBlock.riscvInstructions.getSize(); i++) {
+            RiscvInstruction instr = riscvBlock.riscvInstructions.get(i);
+            for (int idx = 0; idx < instr.getOperandNum(); idx++) {
+                if (instr.isDef(idx) && !(instr instanceof Li)) {
+                    int finalIdx = idx;
+                    map.keySet().removeIf(key -> map.get(key).first.equals(instr.getRegByIdx(finalIdx)));
+                }//删除所有重定义的
+            }
+            if (instr instanceof Li) {
+                // 如果前面有记录这个值，那么就将这个li给删掉，然后将后面的所有使用这个的寄存器都换掉
+                if (map.containsKey(((Li) instr).imm)) {
+                    Reg now = map.get(((Li) instr).imm).first;
+                    Reg def = ((Li) instr).reg;
+                    for (int j = i + 1; j < riscvBlock.riscvInstructions.getSize(); j++) {
+                        RiscvInstruction needReplace = riscvBlock.riscvInstructions.get(j);
+                        if (needReplace instanceof J) continue;
+                        needReplace.replaceUseReg(def, now);
+                    }
+                    map.get(((Li) instr).imm).second = range;// 刷新生存周期
+                    continue;
+                } else {
+                    map.put(((Li) instr).imm, new Pair<>(((Li) instr).reg, range));
+                    map.keySet().removeIf(key -> map.get(key).first.equals(((Li) instr).reg));
+                }
+            }
+            newList.add(instr);
+            HashSet<Integer> needRemove = new HashSet<>();
+            for (Integer key : map.keySet()) {
+                map.get(key).second--;
+                if (map.get(key).second == 0) {
+                    needRemove.add(key);
+                }
+            }
+            for (Integer need : needRemove) {
+                map.remove(need);
+            }
+        }
+        riscvBlock.riscvInstructions.setEmpty();
+        for (RiscvInstruction ri : newList) {
+            riscvBlock.riscvInstructions.addLast(ri);
+        }
+    }
+
+    public static void Mul2SrAdd(RiscvBlock block) {
+
+    }
+
+    public static void icmpBranchToBranch(RiscvBlock block) {
+
+    }
+
+    public static void ConstPointerReUse(RiscvBlock riscvBlock) {
+        final int range = 10;
+        HashMap<RiscvGlobalVar, Pair<Reg, Integer>> map = new HashMap<>();
+        ArrayList<RiscvInstruction> newList = new ArrayList<>();
+        for (int i = 0; i < riscvBlock.riscvInstructions.getSize(); i++) {
+            RiscvInstruction instr = riscvBlock.riscvInstructions.get(i);
+            for (int idx = 0; idx < instr.getOperandNum(); idx++) {
+                if (instr.isDef(idx) && !(instr instanceof La)) {
+                    int finalIdx = idx;
+                    map.keySet().removeIf(key -> map.get(key).first.equals(instr.getRegByIdx(finalIdx)));
+                }//删除所有重定义的
+            }
+            if (instr instanceof La) {
+                // 如果前面有记录这个值，那么就将这个li给删掉，然后将后面的所有使用这个的寄存器都换掉
+                if (map.containsKey((((La) instr).content))) {
+                    Reg now = map.get(((La) instr).content).first;
+                    Reg def = ((La) instr).reg;
+                    for (int j = i + 1; j < riscvBlock.riscvInstructions.getSize(); j++) {
+                        RiscvInstruction needReplace = riscvBlock.riscvInstructions.get(j);
+                        if (needReplace instanceof J) continue;
+                        needReplace.replaceUseReg(def, now);
+                    }
+                    map.get(((La) instr).content).second = range;// 刷新生存周期
+                    continue;
+                } else {
+                    map.put(((La) instr).content, new Pair<>(((La) instr).reg, range));
+                    map.keySet().removeIf(key -> map.get(key).first.equals(((La) instr).reg));
+                }
+            }
+            newList.add(instr);
+            HashSet<RiscvGlobalVar> needRemove = new HashSet<>();
+            for (RiscvGlobalVar key : map.keySet()) {
+                map.get(key).second--;
+                if (map.get(key).second == 0) {
+                    needRemove.add(key);
+                }
+            }
+            for (RiscvGlobalVar need : needRemove) {
+                map.remove(need);
+            }
+        }
+        riscvBlock.riscvInstructions.setEmpty();
+        for (RiscvInstruction ri : newList) {
+            riscvBlock.riscvInstructions.addLast(ri);
+        }
+    }
+}
