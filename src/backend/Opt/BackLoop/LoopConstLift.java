83e21cfa619e4e53b009b800a7c75fab49f64344
何立群 <21373471@buaa.edu.com>
2024-08-15 00:47:28 +0800

    finsih

--- src/backend/Opt/BackLoop/LoopConstLift.java
+++ src/backend/Opt/BackLoop/LoopConstLift.java
@@ -1,10 +1,5 @@
 package backend.Opt.BackLoop;
 
-import backend.Opt.GPpooling.GlobalFloat2roPool;
-import backend.Opt.MemoryOpt.UnknownBaseLSOpt;
-import backend.allocater.LivenessAnalyze;
-import backend.operand.Imm;
-import backend.operand.Reg;
 import backend.riscv.RiscvBlock;
 import backend.riscv.RiscvFunction;
 import backend.riscv.RiscvInstruction.*;
@@ -12,21 +7,15 @@ import backend.riscv.RiscvModule;
 
 import java.util.ArrayList;
 
-import static backend.Opt.MemoryOpt.UnknownBaseLSOpt.records;
-
 public class LoopConstLift {
     // 专属于循环不变量外提
     // 一般属于la与li,剩下的其实都是算循环变量了,都已经在外面实现了
-    // 其实剩下的还有从pool中提取数值,这也算循环不变量
 
     private static int temp_cnt = 0;
-
-    private static final boolean lift_flw_pool = true;
 
     public static void run(RiscvModule riscvModule) {
         for (RiscvFunction function : riscvModule.funcList) {
             if (function.isExternal) continue;
-            LivenessAnalyze.RunOnFunc(function);
             runOnFunc(function);
         }
     }
@@ -38,12 +27,8 @@ public class LoopConstLift {
                 if (!checkOnLoop(function, loop)) break;
             }
         }
-        LivenessAnalyze.RunOnFunc(function);
         for (RiscLoop loop : function.loops) {
             runOnLoop(loop);
-        }
-        for (RiscvBlock block : function.blocks) {
-            runOnBlockBefRA(block);
         }
     }
 
@@ -77,24 +62,9 @@ public class LoopConstLift {
     private static void runOnLoop(RiscLoop riscLoop) {
         ArrayList<RiscvInstruction> arrayList = new ArrayList<>();
         for (RiscvBlock block : riscLoop.blocks) {
-            Reg poolContainer = null;
             for (RiscvInstruction ri : block.riscvInstructions) {
-                if (lift_flw_pool) {
-                    if (ri instanceof La || ri instanceof Li ||
-                            (ri instanceof LS ls && ls.type == LS.LSType.flw && ls.base.equals(poolContainer))) {
-                        arrayList.add(ri);
-                        if (ri instanceof La la && la.content.equals(GlobalFloat2roPool.gPpool)) {
-                            if (poolContainer == null) {
-                                poolContainer = la.reg;
-                            } else {
-                                poolContainer.mergeReg(la.reg);
-                            }
-                        }
-                    }
-                } else {
-                    if (ri instanceof La || ri instanceof Li) {
-                        arrayList.add(ri);
-                    }
+                if (ri instanceof La || ri instanceof Li) {
+                    arrayList.add(ri);
                 }
             }
         }
@@ -112,43 +82,6 @@ public class LoopConstLift {
         }
         for (RiscLoop subLoop : riscLoop.subLoops) {
             runOnLoop(subLoop);
-        }
-    }
-
-
-    private static void runOnBlockBefRA(RiscvBlock block) {
-        ArrayList<LS> ls2move = new ArrayList<>();
-        for (RiscvInstruction instr : block.riscvInstructions) {
-            if (instr instanceof LS ls && ls.base.phyReg != Reg.PhyReg.sp) {
-                long off = ((Imm) ls.addr).getVal();
-                if (ls.type == LS.LSType.ld || ls.type == LS.LSType.lw || ls.type == LS.LSType.flw) {
-                    ArrayList<UnknownBaseLSOpt.UBRecord> list = UnknownBaseLSOpt.queryByOff(ls.val, ls.base, off);
-                    UnknownBaseLSOpt.removeByReg(ls.val);
-                    UnknownBaseLSOpt.removeByBase(ls.val);
-                    if (list.size() != 0) {
-                        ls2move.add(ls);
-                        if (!list.get(0).getReg().equals(ls.val)) list.get(0).getReg().mergeReg(ls.val);
-                    } else {
-                        records.add(new UnknownBaseLSOpt.UBRecord(ls.val, off, ls.base));
-                    }
-                } else {
-                    // 写个简单版的，直接全清空就没事了
-                    records.clear();
-                }
-            } else if (instr instanceof J) {
-                records.clear();
-            } else {
-                for (int i = 0; i < instr.getOperandNum(); i++) {
-                    if (instr.isDef(i)) {
-                        Reg reg = instr.getRegByIdx(i);
-                        UnknownBaseLSOpt.removeByReg(reg);
-                        UnknownBaseLSOpt.removeByBase(reg);
-                    }
-                }
-            }
-        }
-        for (LS ls : ls2move) {
-            ls.remove();
         }
     }
 }
