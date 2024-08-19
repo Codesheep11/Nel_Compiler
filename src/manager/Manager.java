90fb8fdc281b60bc2124504a04ae33cf59fc6fe2
CodeSheep11 <3139784737@qq.com>
2024-08-19 08:36:29 +0800

    111

--- src/manager/Manager.java
+++ src/manager/Manager.java
@@ -113,10 +113,10 @@ public class Manager {
         LoopInterchange.run(module);
         LCSSA.remove(module);
         SCCP();
-        LoopBuildAndNormalize();
-        LoopNestTemp.run(module);
-        LCSSA.run(module);
-        SCCP();
+//        LoopBuildAndNormalize();
+//        LoopNestTemp.run(module);
+//        LCSSA.run(module);
+//        SCCP();
         ConstrainReduce.run(module);
         DeadCodeEliminate();
         LoopBuildAndNormalize();
